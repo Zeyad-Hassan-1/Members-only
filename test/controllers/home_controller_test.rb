d84339a96694ff45df8a14842f5ea3c04f2ddf5a
Zeyad Hasan <studying.zezo@gmail.com>
2025-03-02 13:39:03 +0200

    Update post and user fixtures

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,7 +1,11 @@
 require "test_helper"
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers # Include Devise test helpers
+
   test "should get index" do
+    user = users(:one) # Assuming you have a user fixture
+    sign_in user # Sign in the user
     get home_index_url
     assert_response :success
   end
