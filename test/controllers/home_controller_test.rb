a7ca0b8da6f5419f634dd75d76f1c25cadfcb31e
Zeyad Hasan <studying.zezo@gmail.com>
2025-03-02 13:23:30 +0200

    Update test files and standardize gem syntax

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,12 +1,6 @@
 require "test_helper"
 
-# test/controllers/home_controller_test.rb
 class HomeControllerTest < ActionDispatch::IntegrationTest
-  setup do
-    @user = users(:one) # Load the user fixture
-    @post = Post.create!(title: "Test Post", content: "This is a test post", user_id: @user.id)
-  end
-
   test "should get index" do
     get home_index_url
     assert_response :success
